06bcf3aac4b21512c00838e370759408bdc49ef2
fabiolizzijr <94727592+fabiolizzijr@users.noreply.github.com>
2024-01-25 15:55:18 -0300

    Adding first GUI example

---
+++ GUI/first_GUI.py
@@ -0,0 +1,17 @@
+#First GUI with Tkinter
+#======================
+# Import
+#======================
+import tkinter as tk
+
+# Create a instance
+win = tk.Tk()
+
+# Add a title
+win.title('First Python GUI')
+
+#======================
+# Start GUI
+#======================
+win.geometry("600x600") # Set height 600 and width 600
+win.mainloop()
