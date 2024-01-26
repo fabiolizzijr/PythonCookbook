a8f836f7d247ac4c9ddf4b2e22ccf06bf95d8465
fabiolizzijr <94727592+fabiolizzijr@users.noreply.github.com>
2024-01-26 14:47:37 -0300

    Adding GUI not resizable and  add label examples

---
+++ GUI/GUI_add_label.py
@@ -0,0 +1,23 @@
+# GUI with an label example
+# ttk modules has advanced widgets to make GUI look GREAT!
+# it stands for themed tk - Make things look cool baby!
+# ===========================
+# Imports
+# ===========================
+import tkinter as tk
+from tkinter import ttk # import ttk module from tkinter package
+
+# Create a instance of Tk class
+win = tk.Tk()
+
+# Create a title in GUI
+win.title('GUI with label example')
+
+# Adding a label
+# ttk.Label() constructor
+# ttk.Label(window_instance, text)
+# ttk.Label(win, text="Hello").grid() - calls grid layout manager
+ttk.Label(win, text="A Label").grid(column=0, row=0)
+
+# Start GUI
+win.mainloop()
---
+++ GUI/GUI_not_resizable.py
@@ -0,0 +1,23 @@
+# GUI not resizable example
+# =========================
+# Imports
+# =========================
+import tkinter as tk
+
+# Create an instance of class Tk
+window = tk.Tk()
+
+# Set a title
+window.title('GUI not resizable example')
+
+# Set specific width x height
+window.geometry('350x400')
+
+# Make it not resizable by passing values 0, 0 or False, False
+window.resizable(False,False)
+
+# Enable resize of x-dimension
+# window.resizable(True, False)
+
+# Start GUI
+window.mainloop()
