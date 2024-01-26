3457072c563a0434847eaf40e129a5993f40ba30
fabiolizzijr <94727592+fabiolizzijr@users.noreply.github.com>
2024-01-26 15:06:45 -0300

    Adding GUI with a button that change property as example

---
+++ GUI/GUI_create_button_change_property.py
@@ -0,0 +1,33 @@
+# GUI with a buttons that changes
+# a property of another widget
+# ===============================
+# Imports
+# ===============================
+import tkinter as tk
+from tkinter import ttk
+
+# Create an instance of Tk class
+win = tk.Tk()
+
+# Add a title
+win.title('GUI with button')
+
+# Create a label widget that will be modified
+a_label = ttk.Label(win, text="A new Label")
+a_label.grid(column=0, row=0)
+
+# Button Click Event Function
+def click_me():
+    # Update text of the Button
+    action.configure(text="** I have been Clicked! **")
+    # Update the label, changing the foreground(font color) and text properties
+    a_label.configure(foreground='red')
+    a_label.configure(text="A Red Label")
+
+# Adding a button
+# NOTICE: DO NOT use parentheses, it's event-drive!!!
+action = ttk.Button(win, text="Click me!", command=click_me)
+action.grid(column=1, row=0)
+
+# Start GUI
+win.mainloop()
